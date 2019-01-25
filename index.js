587765003c14f7a861aeb40a7387efc224c91588
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-01-25 16:35:53 -0300

    connect to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
+var mongoose = require("mongoose");
 var api = require("./routes/api");
 var app = express();
 var port = 3000;
@@ -8,6 +9,15 @@ var port = 3000;
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+mongoose.connect(
+  "mongodb://admin:" +
+    process.env.MONGO_ATLAS_PW +
+    "@alvarezgsm-shard-00-00-z43tn.mongodb.net:27017,alvarezgsm-shard-00-01-z43tn.mongodb.net:27017,alvarezgsm-shard-00-02-z43tn.mongodb.net:27017/test?ssl=true&replicaSet=AlvarezGSM-shard-0&authSource=admin&retryWrites=true",
+  {
+    useMongoClient: true
+  }
+);
 
 app.use("/api", api);
 
